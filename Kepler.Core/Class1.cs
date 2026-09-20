6dd3e6fb51556f5561851a35ae30d8ced8822cf9
Melisa Sabatino <sabatinomelisa@gmail.com>
2026-09-20 11:01:10 -0300

    Commit inicial: estructura principal

---
+++ Kepler.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kepler.Core
+{
+    public class Class1
+    {
+    }
+}
